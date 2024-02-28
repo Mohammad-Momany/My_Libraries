0ca0d49dee95ebf195e8f6c63a3341072cb28fc3
Mohammad Almomany <mohammad.w.momani@gmail.com>
2024-02-28 10:24:24 +0300

    Add ReadPositiveNumber Function.

--- Validations.h
+++ Validations.h
@@ -4,28 +4,46 @@
 using namespace std;
 
 namespace Val {
-	int ReadLimitedNumber( int From, int To) {
+    int ReadLimitedNumber(int From, int To) {
 
-    int EnteredNumber = 0;
+        int EnteredNumber = 0;
 
-    while (EnteredNumber > To || EnteredNumber < From || cin.fail()) {
-        cout << "\n Plase enter the number :\n";
-        cin >> EnteredNumber;
+        while (EnteredNumber > To || EnteredNumber < From || cin.fail()) {
+            cout << "\n Plase enter the number :\n";
+            cin >> EnteredNumber;
 
-         if (cin.fail())
-        {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Invalid Number" << endl; 
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid Number" << endl;
+            }
+            else if (EnteredNumber > To)
+                cout << "\nThe number Must be less than " << To << "\n";
+            else if (EnteredNumber < From)
+                cout << "\nThe number Must be greater than " << From << "\n";
+
         }
-        else if (EnteredNumber > To)
-            cout << "\nThe number Must be less than " << To << "\n";
-        else if (EnteredNumber < From)
-            cout << "\nThe number Must be greater than " << From << "\n";
 
-    } 
+        return EnteredNumber;
 
-    return EnteredNumber;
+    }
+    
+    int ReadPositiveNumber(string message) {
 
-}
+        int EnteredNumber = 0;
+
+        do {
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid Number" << endl;
+            }
+            cout << message << endl;
+            cin >> EnteredNumber;
+        } while (EnteredNumber < 0 || cin.fail());
+
+        return EnteredNumber;
+    }
 }
