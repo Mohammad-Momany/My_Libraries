68c21be45fa6e73e98be6c5a094e9b7e9d3465f1
Mohammad Almomany <mohammad.w.momani@gmail.com>
2024-06-22 20:44:29 +0300

    Fix param place

--- Local_Libraries.cpp
+++ Local_Libraries.cpp
@@ -10,6 +10,17 @@ int main()
 
     cout << "\n";
 
+    clsPeriod Period2(clsDate(3, 1, 2022), clsDate(5, 1, 2022));
+    Period2.Print();
+
+
+    //You can check like this
+    cout << Period1.IsOverLapWith(Period2) << endl;
+
+
+    //Also you can call the static method and send period 1 and period 2
+    cout << clsPeriod::IsOverlapPeriods(Period1, Period2) << endl;
+
 
     return 0;
 }
--- clsPeriod.h
+++ clsPeriod.h
@@ -11,7 +11,7 @@ public:
     clsPeriod(clsDate StartDate, clsDate DateTo)
     {
         this->StartDate = StartDate;
-        this->EndDate = EndDate;
+        this->EndDate = DateTo;
 
     }
 
